38d234d6e102369f42e41d960a6fa8401f6e12de
Lehman Black <WileESpaghetti@users.noreply.github.com>
2021-04-25 21:10:22 -0500

    switch to the shaarli fork of NetscapeBookmarkParser The shaarli fork is being actively used and is kept more up to date

--- app/Jobs/ProcessBookmarks.php
+++ app/Jobs/ProcessBookmarks.php
@@ -3,17 +3,15 @@
 namespace App\Jobs;
 
 use App\Bookmark;
-use App\Libraries\NetscapeBookmarkParser;
-use DateTime;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Carbon;
-use Illuminate\Support\Facades\Event;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
+use Shaarli\NetscapeBookmarkParser\NetscapeBookmarkParser;
 
 class ProcessBookmarks implements ShouldQueue
 {
@@ -54,12 +52,14 @@ class ProcessBookmarks implements ShouldQueue
         $bookmarks = $bookmarkParser->parseString($html);
 
         $bookmarks = array_map(function($bookmarkData) {
+            // remove attributes that we do not track
+            unset($bookmarkData['icon']);
+            unset($bookmarkData['tags']); // TODO Consolidate tags and store in separate table
+
             $bookmarkData['user_id'] = $this->user->id;
 
-            unset($bookmarkData['tags']); // FIXME
-
             // Convert from Unix timestamp in microseconds
-            $bookmarkData['created_at'] = Carbon::createFromTimestampMs(trim($bookmarkData['time'] / 1000 )); // FIXME it's in microseconds
+            $bookmarkData['created_at'] = Carbon::createFromTimestampMs(trim($bookmarkData['time']/* / 1000*/ )); // FIXME Google Bookmarks is in microseconds and others might be different
             unset($bookmarkData['time']);
 
             return $bookmarkData;
