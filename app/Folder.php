a91e26536d744b208bdde3934816b076b5a8dc76
Lehman Black <WileESpaghetti@users.noreply.github.com>
2022-07-13 19:26:30 -0500

    Folder class simplification

--- app/Folder.php
+++ app/Folder.php
@@ -1,18 +1,25 @@
 <?php
+declare(strict_types=1);
 
 namespace App;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Folder extends Model
 {
     use HasFactory;
 
     /**
-     * Get the user that owns the phone.
+     * `color` and `links` can be filled only because they are needed for the Larder API
      */
-    public function user()
+    protected $fillable = ['name', 'color', 'links'];
+
+    /**
+     * Get the user that owns the folder.
+     */
+    public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
