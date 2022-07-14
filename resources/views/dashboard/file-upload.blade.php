ba68eae42d939538bad774575b3603f732d7afd0
Lehman Black <WileESpaghetti@users.noreply.github.com>
2022-07-13 19:31:54 -0500

    bootstrap 5 updates for file upload form

--- resources/views/dashboard/file-upload.blade.php
+++ resources/views/dashboard/file-upload.blade.php
@@ -1,25 +1,31 @@
-{{-- TODO have an option for `Create a new folder called 'Imported'` menu option --}}
+{{--
+TODO
+have an option for `Create a new folder called 'Imported'` menu option
+
+TODO
+show max file upload limit
+--}}
 <div class="card mt-3">
     <div class="card-header">{{ __('Upload Bookmarks File') }}</div>
 
     <div class="card-body">
-        @if (session('error'))
+        @if (session('errors'))
             <div class="alert alert-danger" role="alert">
-                {{ session('error') }}
+                {{ session('errors') }}
             </div>
         @endif
         @if(session()->has('success'))
             <div class="alert alert-success">
-                {{ session()->get('success') }}
+                {{ __(session()->get('success')) }}
             </div>
         @endif
-        <form method="POST" action="bookmarks/import" enctype="multipart/form-data">
-            <div class="form-group">
-                <label for="bookmarks">{{ __('Select file to upload') }}</label>
-                <input type="file" class="form-control-file" name="bookmark" id="bookmark" accept="text/html">
-            </div>
 
-            <button type="submit" class="btn btn-primary">Upload</button>
+        <form method="POST" action="bookmarks/import" enctype="multipart/form-data">
+            <label for="bookmark" class="form-label">{{ __('Select file to upload') }}</label>
+            <div class="input-group">
+                <input type="file" class="form-control" name="bookmark" id="bookmark" accept="text/html">
+                <button type="submit" class="btn btn-primary">Upload</button>
+            </div>
 
             @csrf
         </form>
