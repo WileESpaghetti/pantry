ac7a443bbf357ddc01bc64550ad1e8e95d77be8b
Lehman Black <WileESpaghetti@users.noreply.github.com>
2022-12-13 21:21:30 -0500

    use pantry user for registration

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -4,10 +4,10 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
-use App\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Pantry\User;
 
 class RegisterController extends Controller
 {
@@ -60,7 +60,7 @@ class RegisterController extends Controller
      * Create a new user instance after a valid registration.
      *
      * @param  array  $data
-     * @return \App\User
+     * @return \Pantry\User
      */
     protected function create(array $data)
     {
