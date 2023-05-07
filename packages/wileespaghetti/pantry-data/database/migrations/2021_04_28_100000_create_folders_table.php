67f67e972afa54b025ac8669eb63d8698121d1ea
Lehman Black <WileESpaghetti@users.noreply.github.com>
2023-05-06 20:28:45 -0500

    rename pantry data migrations and make use strict mode

--- packages/wileespaghetti/pantry-data/database/migrations/2021_04_28_100000_create_folders_table.php
+++ packages/wileespaghetti/pantry-data/database/migrations/2021_04_28_100000_create_folders_table.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -8,10 +10,8 @@ return new class extends Migration
 {
     /**
      * Run the migrations.
-     *
-     * @return void
      */
-    public function up()
+    public function up(): void
     {
         Schema::create('folders', function (Blueprint $table) {
             $table->id();
@@ -24,10 +24,8 @@ return new class extends Migration
 
     /**
      * Reverse the migrations.
-     *
-     * @return void
      */
-    public function down()
+    public function down(): void
     {
         Schema::dropIfExists('folders');
     }
--- packages/wileespaghetti/pantry-data/database/migrations/2022_11_06_100000_create_bookmarks_table.php
+++ packages/wileespaghetti/pantry-data/database/migrations/2022_11_06_100000_create_bookmarks_table.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -8,10 +10,8 @@ return new class extends Migration
 {
     /**
      * Run the migrations.
-     *
-     * @return void
      */
-    public function up()
+    public function up(): void
     {
         Schema::create('bookmarks', function (Blueprint $table) {
             $table->id();
@@ -27,10 +27,8 @@ return new class extends Migration
 
     /**
      * Reverse the migrations.
-     *
-     * @return void
      */
-    public function down()
+    public function down(): void
     {
         Schema::dropIfExists('bookmarks');
     }
