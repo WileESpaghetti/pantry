a651c044496827d86a0766a183a4581e00e538d4
Lehman Black <WileESpaghetti@users.noreply.github.com>
2023-08-13 20:30:09 -0500

    formatting

--- packages/wileespaghetti/html-bookmarks/src/Services/Formatter.php
+++ packages/wileespaghetti/html-bookmarks/src/Services/Formatter.php
@@ -1,24 +1,26 @@
 <?php
+// FIXME remake into a static Fmt class
 
 declare(strict_types=1);
 
-namespace Pantry\Services;
+namespace HtmlBookmarks\Services;
 
 /**
  * Only does up to Gigabytes
  *
  * @param int $bytes
- * @param $decimals
+ * @param int $decimals
  * @return string
  */
-function humanBytes(int $bytes, $decimals = 1): string {
+function humanBytes(int $bytes, int $decimals = 1): string
+{
     // FIXME strip zeros after the decimal point
     if ($bytes === PHP_INT_MAX) {
         return __('htmlbookmarks::max_file_size_unknown');
     }
 
     $sBytes = strval($bytes);
-    $size = array('B','kB','MB','GB');
+    $size = array('B', 'kB', 'MB', 'GB');
     $factor = floor((strlen($sBytes) - 1) / 3);
     return sprintf("%.{$decimals}f", $sBytes / pow(1024, $factor)) . " {$size[$factor]}";
 }
